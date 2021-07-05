da09d1bb0fa38014597036f3f5aaa645e150842b
Yaniv Suriano <yaniv@skysoftsl.com>
2021-07-05 20:18:33 +0300

    update mockDB and package to install

---
+++ server-app/mockDB.js
@@ -0,0 +1,18 @@
+const mockDB = [
+  {
+    tradingParty: "me",
+    counterParty: "you",
+    amount: "-400",
+  },
+  {
+    tradingParty: "me",
+    counterParty: "you",
+    amount: "500",
+  },
+  {
+    tradingParty: "me",
+    counterParty: "someone_else",
+    amount: "100",
+  },
+];
+module.exports = { mockDB };
